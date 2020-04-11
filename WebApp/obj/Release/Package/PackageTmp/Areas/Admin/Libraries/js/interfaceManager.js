f20a2ea3a1b9c672409f9f33891980a5555c54f5
Ngo Trung Duc <ogggy4858@gmail.com>
2020-04-11 09:56:22 +0700

    jjjjjjjjjjjjjjj

---
+++ WebApp/obj/Release/Package/PackageTmp/Areas/Admin/Libraries/js/interfaceManager.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    $('.btn-banner-add').on('click', function () {
+        if (confirm("Bạn muốn áp dụng bản ghi này")) {
+            var id = $(this).data('id');
+            Active(id, "Banner");
+        }
+    });
+
+    $('.btn-welcome-add').on('click', function () {
+        if (confirm("Bạn muốn áp dụng bản ghi này")) {
+            var id = $(this).data('id');
+            Active(id, "Welcome");
+        }
+    });
+});
+
+
+function Active(id, categoryName) {
+    $.ajax({
+        type: "POST",
+        data: {
+            id: id,
+            categoryName: categoryName
+        },
+        cache: false,
+        async: false,
+        url: "/InterfaceManager/Active/",
+        dataType: 'json',
+        success: function (resp) {
+            if (resp.Status) {
+                location.reload();
+            }
+            else {
+                alert(resp.Message);
+            }
+        }
+    });
+}
